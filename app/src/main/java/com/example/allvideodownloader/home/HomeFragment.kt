16399a70413ba9aa658ac7b4c94f56a2e97d2ad5
mudasirhussains <sahirhussain54@gmail.com>
2024-02-06 01:11:01 +0500

    started home screen

--- app/src/main/java/com/example/allvideodownloader/home/HomeFragment.kt
+++ app/src/main/java/com/example/allvideodownloader/home/HomeFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.example.allvideodownloader.R
 import com.example.allvideodownloader.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
